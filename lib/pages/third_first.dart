20eee9f778471621c99d652a438024a97f069958
Jurody <kamilov.2000@mail.ru>
2021-09-25 11:45:15 +0500

    test

---
+++ lib/pages/third_first.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class ThirdFirst extends StatelessWidget {
+  const ThirdFirst({Key? key}) : super(key: key);
+//Navigator.pushNamed(context, '/third');
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Card(
+        child: Text('data'),
+      )
+    ]);
+  }
+}
