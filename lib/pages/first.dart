57cefc3b167443643ec2672c5a97cfa15b91e233
jurody <kamilov.2000@gmail.com>
2021-12-03 16:47:22 +0500

    updated some libs and fixed main menu

--- lib/pages/first.dart
+++ lib/pages/first.dart
@@ -24,115 +24,117 @@ class FirstPage extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          children: [
-            Card(
-                color: Colors.amber,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/second');
-                      },
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: sizeh / 2,
-                          ),
-                          SizedBox(
-                            width: sizew,
-                            height: sizeh,
-                            child: Text(
-                              AppLocalizations.of(context)!.first_first,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 25,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Card(
+                  color: Colors.amber,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/second');
+                        },
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: sizeh / 2,
+                            ),
+                            SizedBox(
+                              width: sizew,
+                              height: sizeh,
+                              child: Text(
+                                AppLocalizations.of(context)!.first_first,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 25,
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
+                    ],
+                  )),
+              Card(
+                  color: Colors.amber,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/second');
+                    },
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: sizeh / 2,
+                        ),
+                        SizedBox(
+                          width: sizew,
+                          height: sizeh,
+                          child: Text(
+                            AppLocalizations.of(context)!.first_second,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                )),
-            Card(
-                color: Colors.amber,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, '/second');
-                  },
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: sizeh / 2,
-                      ),
-                      SizedBox(
-                        width: sizew,
-                        height: sizeh,
-                        child: Text(
-                          AppLocalizations.of(context)!.first_second,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 25,
+                  )),
+              Card(
+                  color: Colors.amber,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/second');
+                    },
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: sizeh / 2,
+                        ),
+                        SizedBox(
+                          width: sizew,
+                          height: sizeh,
+                          child: Text(
+                            AppLocalizations.of(context)!.first_third,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 25,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                )),
-            Card(
-                color: Colors.amber,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, '/second');
-                  },
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: sizeh / 2,
-                      ),
-                      SizedBox(
-                        width: sizew,
-                        height: sizeh,
-                        child: Text(
-                          AppLocalizations.of(context)!.first_third,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 25,
+                      ],
+                    ),
+                  )),
+              Card(
+                  color: Colors.amber[700],
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/settings');
+                    },
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: sizeh / 2,
+                        ),
+                        SizedBox(
+                          width: sizew,
+                          height: sizeh,
+                          child: Text(
+                            AppLocalizations.of(context)!.settings,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 25,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                )),
-            Card(
-                color: Colors.amber[700],
-                child: InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, '/settings');
-                  },
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: sizeh / 2,
-                      ),
-                      SizedBox(
-                        width: sizew,
-                        height: sizeh,
-                        child: Text(
-                          AppLocalizations.of(context)!.settings,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 25,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                )),
-          ],
+                      ],
+                    ),
+                  )),
+            ],
+          ),
         ),
       ),
     );
