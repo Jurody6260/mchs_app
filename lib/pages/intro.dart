f0d87e9b9c6f478342173d6bcc5a93fa9f313841
jurody <kamilov.2000@gmail.com>
2022-04-22 18:01:49 +0500

    added url to intro

--- lib/pages/intro.dart
+++ lib/pages/intro.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Intro extends StatelessWidget {
   const Intro({Key? key}) : super(key: key);
@@ -46,6 +47,19 @@ class Intro extends StatelessWidget {
                           fontSize: sizefs,
                         ),
                       ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: ElevatedButton(
+                            child: Text(
+                              AppLocalizations.of(context)!.intro_url,
+                            ),
+                            onPressed: () {
+                              launch(AppLocalizations.of(context)!.intro_url);
+                            },
+                          ),
+                        ),
+                      )
                     ],
                   ),
                 ),
