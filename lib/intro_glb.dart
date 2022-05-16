f79900296c8050e74408b3b823b3635b05418d8c
Jurody <kamilov.2000@mail.ru>
2022-05-16 15:33:14 +0500

    lang select after globus

--- lib/intro_glb.dart
+++ lib/intro_glb.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:final_mchs_app/pages/first.dart';
+import 'package:final_mchs_app/pages/lang_select_intro.dart';
 import 'package:flutter/material.dart';
 
 class IntroGlobus extends StatelessWidget {
@@ -18,7 +19,7 @@ void alo(context) async {
   Timer(Duration(seconds: 5), () {
     Navigator.pushAndRemoveUntil(
       context,
-      MaterialPageRoute(builder: (context) => FirstPage()),
+      MaterialPageRoute(builder: (context) => LangSelectIntro()),
       (Route<dynamic> route) => false,
     );
   });
