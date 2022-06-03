c0609c7cbda416039b357533b3bbfcef74fe8c2c
jurody <kamilov.2000@gmail.com>
2022-06-03 12:03:38 +0500

    scaff

--- lib/additional_four.dart
+++ lib/additional_four.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import '../funcs/mybar.dart';
 
 class AdditionaFour extends StatelessWidget {
   const AdditionaFour({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(appBar: myBar(), body: SingleChildScrollView());
   }
 }
