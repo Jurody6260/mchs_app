81b005a2b3bc1ac94f6ccf759e2a755362f84874
jurody <kamilov.2000@gmail.com>
2022-06-13 12:16:51 +0500

    unused imports

--- lib/pages/lang_select.dart
+++ lib/pages/lang_select.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:final_mchs_app/provider.dart';
-import 'package:final_mchs_app/pages/first.dart';
-
 import '../funcs/mybar.dart';
 import '../main.dart';
 
