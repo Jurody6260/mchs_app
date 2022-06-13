f81c9b52e8516660952f53c990d44788d321cab7
jurody <kamilov.2000@gmail.com>
2022-06-13 12:16:05 +0500

    added page

--- lib/additional_four.dart
+++ lib/additional_four.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../funcs/mybar.dart';
 
 class AdditionaFour extends StatelessWidget {
@@ -6,6 +8,48 @@ class AdditionaFour extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: myBar(), body: SingleChildScrollView());
+    double sizefs = 18;
+    return Scaffold(
+      appBar: myBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Card(
+                color: Colors.amber[100],
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        AppLocalizations.of(context)!.additional_four_1,
+                        style: TextStyle(
+                          fontSize: sizefs,
+                        ),
+                      ),
+                      // Padding(
+                      //   padding: const EdgeInsets.all(8.0),
+                      //   child: Center(
+                      //     child: ElevatedButton(
+                      //       child: Text(
+                      //         AppLocalizations.of(context)!.after_url_3,
+                      //       ),
+                      //       onPressed: () {
+                      //         launch(AppLocalizations.of(context)!.after_url_3);
+                      //       },
+                      //     ),
+                      //   ),
+                      // )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
